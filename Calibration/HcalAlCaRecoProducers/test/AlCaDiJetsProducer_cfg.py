62bd124db9a8a931e25d6fdc62cd46e134d42fdc
David Sheffield <sheffield@physics.rutgers.edu>
2015-01-30 10:13:18 -0600

    Switch to RelVal samples and add reports.

--- Calibration/HcalAlCaRecoProducers/test/AlCaDiJetsProducer_cfg.py
+++ Calibration/HcalAlCaRecoProducers/test/AlCaDiJetsProducer_cfg.py
@@ -12,13 +12,16 @@ process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(True)
 )
 
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
-)
-process.source = cms.Source("PoolSource",
-    fileNames = 
-cms.untracked.vstring('/store/relval/CMSSW_7_3_0/RelValPhotonJets_Pt_10_13/GEN-SIM-RECO/MCRUN2_73_V7-v1/00000/522CE329-7B81-E411-B6C3-0025905A6110.root')
-)
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.MessageLogger.cerr.FwkReport.reportEvery=cms.untracked.int32(1000)
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
+        '/store/relval/CMSSW_7_3_0/RelValQCD_Pt_80_120_13/GEN-SIM-RECO/MCRUN2_73_V9_71XGENSIM_FIXGT-v1/00000/8E758AAA-4DA2-E411-8068-003048FFCB96.root',
+        '/store/relval/CMSSW_7_3_0/RelValQCD_Pt_80_120_13/GEN-SIM-RECO/MCRUN2_73_V9_71XGENSIM_FIXGT-v1/00000/CE0DAE28-56A2-E411-AEFF-003048FFD79C.root',
+        '/store/relval/CMSSW_7_3_0/RelValQCD_Pt_80_120_13/GEN-SIM-RECO/MCRUN2_73_V9_71XGENSIM_FIXGT-v1/00000/D4D21D16-56A2-E411-A0C4-0026189438E2.root'
+))
 
 process.load("Calibration.HcalAlCaRecoProducers.alcadijets_cfi")
 
